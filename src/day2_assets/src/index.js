2305c34d8df1d22d6650d4475399846896783631
MJ <kelvinzou@outlook.com>
2022-03-06 16:20:07 -0600

    Initial commit.

---
+++ src/day2_assets/src/index.js
@@ -0,0 +1,19 @@
+import { day2 } from "../../declarations/day2";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await day2.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
